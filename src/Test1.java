bd18459bba3f281a671e8366a8f4cb6015a79325
Dulyana-Silva <ddulyanasilva@gmail.com>
2025-06-28 23:26:10 +0530

    Declaration & Initialization

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+public class Test1{
+    public static void main (String [] args){
+        //Declaration and Initialization
+        int x= 10;
+        String name ="Java";
+        boolean bool = true;
+        float y= 2.25;
+        char z ='A';
+
+        //Print Values
+        System.out.println(x);
+        System.out.println(name);
+        System.out.println(bool);
+        System.out.println(y);
+        System.out.println(z);
+        
+    }
+}
