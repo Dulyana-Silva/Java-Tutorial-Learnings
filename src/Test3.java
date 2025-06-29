52985946bc07cda68b4a6528d6c7a3509142e40b
Dulyana-Silva <ddulyanasilva@gmail.com>
2025-06-29 14:49:03 +0530

    Mathematical Operators

---
+++ src/Test3.java
@@ -0,0 +1,22 @@
+public class Test3{
+    public static void main(String args []){
+
+        int x,y, z,add,sub,multi ,div,remainder;
+        x= 10;
+        y=5;
+        z=11;
+        add= x+y;
+        sub= x-y;
+        multi = x*y;
+        div=x/y;
+        remainder =z%y;
+        
+        System.out.println("Answer is "+ add);
+        System.out.println("Answer is "+ sub);
+        System.out.println("Answer is "+ multi);
+        System.out.println("Answer is "+ div);
+        System.out.println("Answer is "+ remainder);
+
+
+    }
+}
