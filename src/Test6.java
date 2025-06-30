23377a47a42ff7ac17a00b6dd2e50bf85b83d1df
Dulyana-Silva <ddulyanasilva@gmail.com>
2025-06-30 14:36:52 +0530

    Logical Operators

---
+++ src/Test6.java
@@ -0,0 +1,21 @@
+public class Test6 {
+    public static void main(String [] args){
+        int x = 10;
+        int y = -20;
+
+        if (x>0 && y>0)  // AND Operator
+              { System.out.println("Positive");
+        }
+        else {  System.out.println("Negative");
+        }
+
+         if (x>0 || y>0)  // OR Operator
+              { System.out.println("Positive");
+        }
+        else {  System.out.println("Negative");
+        }
+
+    }
+
+    
+}
