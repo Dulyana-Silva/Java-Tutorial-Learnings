f377ec68801d2a2de10a8e144ded184c0eb17293
Dulyana-Silva <ddulyanasilva@gmail.com>
2025-06-30 20:35:34 +0530

    Java While Loop

---
+++ src/Test8.java
@@ -0,0 +1,23 @@
+public class Test8 {
+    public static void main(String args []){
+
+        /* while (true){
+        System.out.println("  This is an Infinite Loop   ")
+        }
+        */ 
+
+        int m = 1;
+        while (m <= 5){
+            System.out.println("Hello");
+            m++;
+        }
+
+        int i = 10;
+        while (i<= 20){
+            System.out.println(i);
+            i+=2;  // i = i+ 2
+        }
+    
+    }
+    
+}
