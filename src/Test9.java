0e6896db34a2c842513c7a92f168151316a93bbc
Dulyana-Silva <ddulyanasilva@gmail.com>
2025-06-30 20:58:10 +0530

    Java Do While Loop

---
+++ src/Test9.java
@@ -0,0 +1,23 @@
+public class Test9 {
+    public static void main(String args []){
+
+        int i = 1;
+        do {
+            System.out.println("Hello");
+            i++;
+        }
+        while (i <= 6);     
+        
+//Different betwween Do While & While---> while loop 1st checks the condition and do the task only if it's SATISFIED
+// Do while loop will always do the task once and then continues if only the condition is true
+        
+        int x= 1;
+        do {
+            System.out.println("Positive");
+            x++;
+        }
+        while (x>10);
+        
+    }
+    
+}
