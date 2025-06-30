ef927ce14b9071f75f9325fa6fd2c586b4d7b59b
Dulyana-Silva <ddulyanasilva@gmail.com>
2025-06-30 13:24:56 +0530

    Comparison Operators

---
+++ src/Test5.java
@@ -0,0 +1,22 @@
+public class Test5 {
+    public static void main(String args[]){
+
+       int x= 12;
+       if (x== 10){
+            System.out.println("x is equal to 10");
+       }
+       else if(x>13){
+            System.out.println("x is greater than 13");
+       }
+       else if (x <= 9){
+            System.out.println("x is lass than or equal to 9");
+       }
+       else if (x <= 11){
+            System.out.println("x is less than or equal to 11");
+       }
+       else {
+            System.out.println("x is 12");
+       }
+    }
+    
+}
