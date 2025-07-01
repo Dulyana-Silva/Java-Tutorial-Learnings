4c7718efd6a7ab0e54112693f2e1697115fa65e9
Dulyana-Silva <ddulyanasilva@gmail.com>
2025-07-01 13:24:56 +0530

    Java For Loop

---
+++ src/Test11.java
@@ -0,0 +1,20 @@
+public class Test11 {
+    public static void main(String args []){
+
+        for (int x =1; x<=10; x++){
+            System.out.println(x);
+        }
+
+
+        for (int a = 5; a>=1; a--){
+            System.out.println("Hello");
+        }
+
+        String [] names= {"Lasal", "Chanudi","Mishara"};
+        for (int i=0; i<names.length ; i++){
+            System.out.println(names[i]);
+        }
+
+    }
+    
+}
