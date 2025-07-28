6e7d19ce42f75fc12072d427ebbe9e7802852c22
Dulyana-Silva <ddulyanasilva@gmail.com>
2025-07-28 12:09:37 +0530

    polymorphism method overloading

--- src/Test22/Meth_Overloading.java
+++ src/Test22/Meth_Overloading.java
@@ -1,6 +1,5 @@
 package Test22;
-// In this class we discuss Method Overloading in polymorphism
-// The number of variables passed or variable's data types should be different
+
 public class Meth_Overloading {
 
     public void meth(){
@@ -18,9 +17,9 @@ public class Meth_Overloading {
 
     public static void main(String[] args) {
         Meth_Overloading obj = new Meth_Overloading();
-        obj.meth(); // method 1 is printed as there's no parameters passed
-        obj.meth(1); // method 2 is printed as an int value is passed
+        obj.meth();        // method 1 is printed as there's no parameters passed
+        obj.meth(1);     // method 2 is printed as an int value is passed
         obj.meth(2,3); // method 3 is printed as two int values are passed
-        obj.meth(4.45); // method 4 is printed as a double is passed
+        obj.meth(4.45);  // method 4 is printed as a double is passed
     }
 }
