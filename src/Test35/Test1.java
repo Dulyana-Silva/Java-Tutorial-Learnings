eeb82cccaa53ecc466478b9fb5e65626fe78fad3
Dulyana-Silva <ddulyanasilva@gmail.com>
2025-09-18 18:36:39 +0530

    Another way instead of Generics

--- src/Test35/Test1.java
+++ src/Test35/Test1.java
@@ -6,9 +6,10 @@ package Test35;
 //It's the TOP class in java hierachy, So every dataType is under this Object Class
 
 class A{                                        
-    void print(Object x){             
+    void print(Object x){    //if we're going to store the values of the passed object in a variable then everytime we have to do type casting   
+                             //but when doing this we might get runtime errors, which are even hard to identify, therefore our solution is Generics
         // Double y = (Double)x;     //Type Casting
-
+        // String y = (String)x;     //Type Casting
 
         System.out.println(x);   
     }
@@ -18,8 +19,8 @@ public class Test1{
     public static void main(String [] args){
         A obj1 = new A();
         obj1.print("Hello");   //Hello
-        // obj1.print("20");      //20
-        // obj1.print("20.223");  //20.223
+        obj1.print("20");      //20
+        obj1.print("20.223");  //20.223
 
 
 
